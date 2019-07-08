39902a6f43f6c9657d8e26842243d75ab1168d90
EvgeniaNikolaeva <52248706+EvgeniaNikolaeva@users.noreply.github.com>
2019-07-08 08:19:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-alert('Hello Mediasoft!');
+function calculator(){
+	let a = +prompt('Введите число', '');
+	let b = +prompt('Введите число','');
+	let c = prompt('Ведите операцию','');
+	let operation = 0;
+		if (c == '+') {
+			operation = a + b;
+		}else if (c== '-')
+		{
+		 	operation = a - b;			
+		}
+		if (c== '*'){
+			operation = a * b;
+		}else if (c== '/'){
+			operation = a / b;
+		}
+		alert(operation);		
+}
+calculator();
+      
